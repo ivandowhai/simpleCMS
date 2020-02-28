45b218559e977c3f1c3a70c10721cfbfc04c62d2
idovhai <ivan@snov.io>
2020-02-28 16:15:05 +0200

    Little refactor

--- app/core/Database.go
+++ app/core/Database.go
@@ -6,8 +6,8 @@ import (
 )
 
 func GetDB() *sql.DB {
-	var settings *Settings = GetSettings()
-	var dataSourceName string = settings.DBUser + ":" + settings.DBPassword + "@" + settings.DBHost + "/" + settings.DBName
+	var settings = GetSettings()
+	var dataSourceName = settings.DBUser + ":" + settings.DBPassword + "@" + settings.DBHost + "/" + settings.DBName
 
 	db, err := sql.Open(settings.DBDriver, dataSourceName)
 	if err != nil {
