f2ffd1ba1b07d9eb31c479f3e725f509b6a88ab6
idovhai <ivan@snov.io>
2020-03-05 09:32:27 +0200

    Fix session

--- app/contollers/auth/LoginController.go
+++ app/contollers/auth/LoginController.go
@@ -20,6 +20,7 @@ func LoginPage(writer http.ResponseWriter, _ *http.Request) {
 }
 
 func Login(writer http.ResponseWriter, request *http.Request) {
+	// TODO: log all errors
 	templ, err := template.ParseFiles("templates/default/auth/login.html")
 	if err != nil {
 		fmt.Println(err)
@@ -41,6 +42,7 @@ func Login(writer http.ResponseWriter, request *http.Request) {
 	}
 
 	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(request.Form.Get("password"))); err != nil {
+		fmt.Println(err.Error())
 		data.Result = "password is wrong"
 		templ.Execute(writer, data)
 		return
@@ -53,8 +55,11 @@ func Login(writer http.ResponseWriter, request *http.Request) {
 	}
 
 	session.Values["userID"] = user.ID
-
-	fmt.Println(user.ID)
+	err = session.Save(request, writer)
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
 	data.Result = "OK"
 	templ.Execute(writer, data)
--- app/repositories/user/GetUserByEmailAndPassword.go
+++ app/repositories/user/GetUserByEmailAndPassword.go
@@ -7,10 +7,10 @@ import (
 )
 
 func GetByEmail(email string) (*models.User, error) {
-	row := core.GetDB().QueryRow("select id, name, email, role from users where email = :email", email)
+	row := core.GetDB().QueryRow("select id, name, email, password, role from users where email = ?", email)
 
 	user := new(models.User)
-	err := row.Scan(&user.ID, &user.Name, &user.Email, &user.Role)
+	err := row.Scan(&user.ID, &user.Name, &user.Email, &user.Password, &user.Role)
 	if err != nil {
 		return &models.User{}, errors.New("user not found")
 	}
