7af673a63e2400d2aed1baa73a96993ee2b14aab
idovhai <ivan@snov.io>
2020-03-11 10:36:33 +0200

    Start simple bootstrap stiling

--- app/core/View.go
+++ app/core/View.go
@@ -6,7 +6,6 @@ import (
 )
 
 func GetView(name string) *template.Template {
-	// TODO: move path to config
 	templ, err := template.New("").
 		ParseFiles("templates/"+GetSettings().Template+"/"+name+".html", "templates/"+GetSettings().Template+"/layout/main.html")
 	if err != nil {
--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 )
 
 func main() {
+	fileServer := http.FileServer(http.Dir("./templates/default/assets"))
+	http.Handle("/static/", http.StripPrefix("/static/", fileServer))
 	http.Handle("/", routes.GetRouter())
 	http.ListenAndServe(":8080", nil)
 }
