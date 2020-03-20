8c31fb666a7880d2cd1b5c8b78399883841a3a05
idovhai <ivan@snov.io>
2020-03-20 12:18:32 +0200

    Refactor

--- app/contollers/admin/AdminController.go
+++ app/contollers/admin/AdminController.go
@@ -4,6 +4,7 @@ import (
 	"../../core"
 	"../../models"
 	"../../repositories"
+	"../../services"
 	"github.com/gorilla/mux"
 	"net/http"
 	"strconv"
@@ -57,54 +58,18 @@ func UserEdit(writer http.ResponseWriter, request *http.Request) {
 }
 
 func UserUpdate(writer http.ResponseWriter, request *http.Request) {
-	logger := core.Logger{}
-	logger.Init()
-	userRepository := repositories.UserRepository{}
-
-	ID, err := strconv.ParseUint(mux.Vars(request)["userId"], 10, 16)
-	if err != nil {
-		logger.WriteLog(err.Error(), "error")
-		return
-	}
-
-	user, err := userRepository.GetById(ID)
-	if err != nil {
-		logger.WriteLog(err.Error(), "error")
-		return
-	}
-
+	userService := services.UserService{}
 	request.ParseForm()
-	role, err := strconv.ParseUint(request.Form.Get("role"), 10, 16)
-	if err != nil {
-		logger.WriteLog(err.Error(), "error")
-		return
-	}
 
-	user.Role = uint8(role)
-
-	userRepository.ChangeRole(user)
+	userService.ChangeRole(mux.Vars(request)["userId"], request.Form.Get("role"))
 
 	http.Redirect(writer, request, "/admin/users", http.StatusSeeOther)
 }
 
 func UserDelete(writer http.ResponseWriter, request *http.Request) {
-	logger := core.Logger{}
-	logger.Init()
-	userRepository := repositories.UserRepository{}
+	userService := services.UserService{}
 
-	ID, err := strconv.ParseUint(mux.Vars(request)["userId"], 10, 16)
-	if err != nil {
-		logger.WriteLog(err.Error(), "error")
-		return
-	}
-
-	user, err := userRepository.GetById(ID)
-	if err != nil {
-		logger.WriteLog(err.Error(), "error")
-		return
-	}
-
-	userRepository.Delete(user)
+	userService.Delete(mux.Vars(request)["userId"])
 
 	http.Redirect(writer, request, "/admin/users", http.StatusSeeOther)
 }
---
+++ app/services/UserService.go
@@ -0,0 +1,57 @@
+package services
+
+import (
+	"../core"
+	"../repositories"
+	"github.com/gorilla/mux"
+	"strconv"
+)
+
+type UserService struct{}
+
+func (s *UserService) ChangeRole(userIdStr string, roleStr string) {
+	logger := core.Logger{}
+	logger.Init()
+	userRepository := repositories.UserRepository{}
+
+	ID, err := strconv.ParseUint(userIdStr, 10, 16)
+	if err != nil {
+		logger.WriteLog(err.Error(), "error")
+		return
+	}
+
+	user, err := userRepository.GetById(ID)
+	if err != nil {
+		logger.WriteLog(err.Error(), "error")
+		return
+	}
+
+	role, err := strconv.ParseUint(roleStr, 10, 16)
+	if err != nil {
+		logger.WriteLog(err.Error(), "error")
+		return
+	}
+
+	user.Role = uint8(role)
+
+	userRepository.ChangeRole(user)
+}
+
+func (s *UserService) Delete(userIdStr string) {
+	logger := core.Logger{}
+	logger.Init()
+	userRepository := repositories.UserRepository{}
+	ID, err := strconv.ParseUint(userIdStr, 10, 16)
+	if err != nil {
+		logger.WriteLog(err.Error(), "error")
+		return
+	}
+
+	user, err := userRepository.GetById(ID)
+	if err != nil {
+		logger.WriteLog(err.Error(), "error")
+		return
+	}
+
+	userRepository.Delete(user)
+}
